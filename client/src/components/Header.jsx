68b620ec7fede5596f621ce800f8c11012a164d1
Anas Hasna <hasnaanas230@gmail.com>
2023-07-10 13:40:30 +0100

    header commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>
+        <p
+          style={{ fontWeight: "bold", textAlign: "center", margin: "2rem" }}
+          className="text-muted">
+          TODO APP
+        </p>
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
